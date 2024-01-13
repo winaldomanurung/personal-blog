e7a4044b61059b1c9798f7eb5e522a65f6398ba9
winaldomanurung <winaldo.code@gmail.com>
2024-01-13 18:53:54 +0700

    edit styling for password dialog

--- components/Password.tsx
+++ components/Password.tsx
@@ -16,19 +16,19 @@ export default function Password({ onSubmitPassword }) {
   }
 
   return (
-    <div className="fixed inset-0 z-10 w-screen overflow-y-auto bg-slate-300 bg-opacity-25">
-      <div className="flex min-h-full items-center justify-center p-4 text-center  sm:p-0">
+    <div className="fixed inset-0 z-10 w-screen overflow-y-auto  bg-opacity-25">
+      <div className="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
         <div>
           <form
             className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
             onSubmit={handleSubmit}
           >
-            <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+            <div className="bg-gray-50 px-4 pb-4 pt-5 dark:bg-gray-900/70 dark:shadow-gray-800/40 sm:p-6 sm:pb-4">
               <div className="sm:flex sm:items-start">
                 <div className="mt-3 text-center sm:ml-4 sm:mr-4 sm:mt-0 sm:text-left">
-                  <div className="text-base font-semibold leading-6 text-gray-900">⚠️ Password</div>
+                  <div className="text-base font-bold uppercase leading-6">⚠️ Password</div>
                   <div className="mt-2">
-                    <p className="text-sm text-gray-500">
+                    <p className="text-sm text-gray-700 hover:text-primary-500 dark:text-gray-300">
                       This article is protected. Please type the password so we can give you the
                       access to read it.
                     </p>
@@ -42,7 +42,9 @@ export default function Password({ onSubmitPassword }) {
                         type="password"
                         name="password"
                         id="password"
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:bg-gray-500/40 dark:text-gray-300 dark:ring-gray-400 placeholder:dark:text-red-100 sm:text-sm
+                        sm:leading-6
+                        "
                         placeholder="Type password..."
                         value={password}
                         onChange={handleChange}
@@ -59,16 +61,16 @@ export default function Password({ onSubmitPassword }) {
               </div>
             </div>
 
-            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-10">
+            <div className="bg-gray-100 px-4 py-3 dark:bg-gray-950/70 dark:shadow-gray-950/40 sm:flex sm:flex-row-reverse sm:px-10">
               <button
                 type="submit"
-                className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                className="inline-flex w-full justify-center rounded-md bg-primary-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 sm:ml-3 sm:w-auto"
               >
                 Submit
               </button>
               <button
                 type="button"
-                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto "
                 onClick={() => router.back()}
               >
                 Cancel
