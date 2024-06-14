00186923e7803ebada27aefc7ce84e15a47ea712
winaldomanurung <winaldo.code@gmail.com>
2024-06-14 17:48:54 +0700

    add 2 articles

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,18 +1,30 @@
 const projectsData = [
+  // {
+  //   title: 'Kodepatra',
+  //   description: `What if I could document and share what I've learned in programming to the world? Insights, best practice, projects
+  //   and more, through videos.
+  //   Kodepatra is the platform to help me doing exactly that.`,
+  //   imgSrc: '/static/images/kodepatra.png',
+  //   href: 'https://www.youtube.com/@kodepatra',
+  // },
+  // {
+  //   title: 'Coding Course',
+  //   description: `As a completely self-learner in this software development field, I know how hard is that to define what you should learn, what's the best source, what's the next step, etc. With this coding course, you can start learning to code in a more structured way.`,
+  //   imgSrc: '/static/images/coding-course.png',
+  //   href: '/blog/coding/kodepatra',
+  // },
   {
-    title: 'Kodepatra',
-    description: `What if I could document and share what I've learned in programming to the world? Insights, best practice, projects
-    and more, through videos. 
-    Kodepatra is the platform to help me doing exactly that.`,
-    imgSrc: '/static/images/kodepatra.png',
-    href: 'https://www.youtube.com/@kodepatra',
+    title: 'Under Construction',
+    description: `Under Construction`,
+    imgSrc: '/static/images/construction.png',
+    href: '#',
   },
-  {
-    title: 'Coding Course',
-    description: `As a completely self-learner in this software development field, I know how hard is that to define what you should learn, what's the best source, what's the next step, etc. With this coding course, you can start learning to code in a more structured way.`,
-    imgSrc: '/static/images/coding-course.png',
-    href: '/blog/coding/kodepatra',
-  },
+  // {
+  //   title: 'Coding Course',
+  //   description: `As a completely self-learner in this software development field, I know how hard is that to define what you should learn, what's the best source, what's the next step, etc. With this coding course, you can start learning to code in a more structured way.`,
+  //   imgSrc: '/static/images/coding-course.png',
+  //   href: '/blog/coding/kodepatra',
+  // },
 ]
 
 export default projectsData
